22cb02520c0797af41277497138f622ad3c2c539
Sourin <iamsourinsarkar@gmail.com>
2024-01-11 15:12:51 +0530

    Learned Utils. Made ApiError and ApiResponse

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,29 @@
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((error) => next(error))
+    }
+}
+
+export { asyncHandler }
+
+
+
+
+
+
+
+
+
+
+
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(error.code).json({
+//             success: false,
+//             message: error.message,
+//         })
+//     }
+// }
