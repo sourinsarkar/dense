efa34d7a5edcd8c8195d41b2d7d15af955783bc2
Sourin <iamsourinsarkar@gmail.com>
2024-01-12 21:12:03 +0530

    Issues Resolved

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,4 @@
-import { asyncHandler } from "../utils/asyncHandler";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (res, req) => {
     res.status(200).json({
