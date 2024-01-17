39d6f8e6902acbf49f499a96d8415b33848d4fd2
Sourin <iamsourinsarkar@gmail.com>
2024-01-17 13:00:39 +0530

    Added register controller and bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,8 @@
-import { asyncHandler } from "../utils/asyncHandler.js";
+import { asyncHandler } from "../utils/asyncHandler.js"
+import { ApiError } from "../utils/ApiError.js"
+import { User } from "../models/user.models.js"
+import { uploadOnCloudinary } from "../utils/cloudinary.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
 
 const registerUser = asyncHandler(async (req, res) => {
     // res.status(200).json({
@@ -17,6 +21,57 @@ const registerUser = asyncHandler(async (req, res) => {
     
     const { fullName, email, username, password } = req.body
     console.log("email: ", email);
+
+    // if(fullName === "") {
+    //     throw new ApiError(400, "Full name is requried");
+    // }
+    
+    if(
+        [fullName, email, username, password].some((field) =>
+        field?.trim() === "")
+    ) {
+        throw new ApiError(400, "Full name is requried");
+    }
+
+    const existingUser = User.findOne({
+        $or: [{username}, {email}]
+    })
+
+    if(existingUser) {
+        throw new ApiError(409, "User with email or username already exists.")
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    if(!avatarLocalPath)
+        throw new ApiError(400, "Avatar is required.");
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if(!avatar)
+        throw new ApiError(400, "Avatar is required.");
+
+    const user = await User.create({
+        fullName,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        username: username.toLowerCase(),
+        password, 
+    })
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if(!createdUser)
+        throw new ApiError(500, "Something went wrong while registering the user.")
+
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "User registered successfully.")
+    )
 })
 
 export { registerUser }
--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -14,7 +14,7 @@ class ApiError extends Error {
         
         if(stack) {
             this.stack = stack;
-        } else {
+        } else {    
             Error.captureStackTrace(this, this.constructor)
         }
     }    
