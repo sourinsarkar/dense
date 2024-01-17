e209089f52eff66fb2a5ae1df1da8a2db8b1ce16
Sourin <iamsourinsarkar@gmail.com>
2024-01-17 11:42:21 +0530

    Testing for user details

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,22 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: "ok"
-    })
+    // res.status(200).json({
+    //     message: "OK"
+    // })
+
+    // get user details frmo frontend
+    // validation
+    // check if user is unique (via username and email)
+    // check for images, check for avatar
+    // upload them to cloudinary, avatar
+    // create user object - create entry in db
+    // remove password and refresh token from response
+    // check for user creation
+    // if yes then return response
+    
+    const { fullName, email, username, password } = req.body
+    console.log("email: ", email);
 })
 
 export { registerUser }
