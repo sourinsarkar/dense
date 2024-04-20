f72e6275bee24f6bce82c7127689db9f3e42426c
Sourin <iamsourinsarkar@gmail.com>
2024-04-21 01:10:05 +0530

    Branching it off

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,8 +2,6 @@ import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
-
-
 const router = Router();
 
 router.route("/register").post(
@@ -20,4 +18,4 @@ router.route("/register").post(
     registerUser)
 
 
-export default router
+export default router   
